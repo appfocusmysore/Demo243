5da7048f1b5ad07b58f23c9753dfe59844966b82
RKR <info@appfocus.in>
2016-10-18 14:45:20 +0530

    Initial commit

---
+++ src/com/company/Utilities.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by RKR on 10/18/2016.
+ */
+public class Utilities {
+
+    public static boolean isUserValid(int id, int code){
+        //to do
+        return true;
+    }
+}
